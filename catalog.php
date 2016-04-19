413b7d3b3fda00b0efb155f090874a82995a6732
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-19 19:23:44 +0100

    Set section variable for each desired page, and use conditional to check active page

--- catalog.php
+++ catalog.php
@@ -4,14 +4,21 @@
 //which will display the full catalog, all the books,films, music. in the library
 $pageTitle = "Full Catalog";
 
+//by default we are not showing any of our catagories we are just showing the full
+//catalog so we don't need anything to be underlined
+$section = null;
+
 //first check if get variable exists
 if(isset($_GET["cat"])){
     if($_GET["cat"] == "books"){
         $pageTitle = "Books";
+        $section = "books";
     }else if($_GET["cat"] == "movies"){
         $pageTitle = "Movies";
+        $section = "movies";
     }else if($_GET["cat"] == "music"){
         $pageTitle = "Music";
+        $section = "music";
     }
 }
 
