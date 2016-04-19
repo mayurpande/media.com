7926f87b155408368d1b27be121a94f4101c9cf1
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-19 18:48:41 +0100

    used get variable to set page title if condition is true

--- catalog.php
+++ catalog.php
@@ -1,12 +1,25 @@
-<?php 
+<?php
+//default value if none of condition are met
+//if there is no value set for the variable in the query string we will display this
+//which will display the full catalog, all the books,films, music. in the library
 $pageTitle = "Full Catalog";
+
+if($_GET["cat"] == "books"){
+    $pageTitle = "Books";
+}else if($_GET["cat"] == "movies"){
+    $pageTitle = "Movies";
+}else if($_GET["cat"] == "music"){
+    $pageTitle = "Music";
+}
+
+
 
 include('inc/header.php'); 
 
 ?>
 
 <div class="section page">
-    <h1>Full catalog</h1>
+    <h1><?php echo $pageTitle ?></h1>
 </div>
 
 
