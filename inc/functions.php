88b7667644a60ef5c5b50d014ee9e63c275df1dd
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-22 13:05:23 +0100

    Created function to return categories list

--- inc/functions.php
+++ inc/functions.php
@@ -9,3 +9,24 @@ function get_item_html($id,$item){
    
    return $output;
 }
+
+//fn to return array keys for catalog
+//we pass in arguements catalog array and the category we wish to return
+function array_category($catalog,$catagory){
+    //we want to return an array so we start by creating an empty array named output
+    $output = array();
+
+    //we need to loop through our catalog array, so once again we create a foreach loop
+
+    foreach($catalog as $id => $item) {
+        //foreach item we need to if the category is the one that matches our category
+        //parameter 
+        if(strtolower($category) == strtolower($item["category"])){
+            //if the category we passed in as our arugement matches the category of the 
+            //current item we are looping through, we need to add the id to the o/p array
+            $output[] = $id;
+        }
+    }
+    return $output;
+    
+}
