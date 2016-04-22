f0c66807dea4f31f3c81228db6d19b4aaf3efb03
Mayur Pande <mayurpande.uk@gmail.com>
2016-04-22 10:09:49 +0100

    Created simple catalog array

--- catalog.php
+++ catalog.php
@@ -1,4 +1,14 @@
 <?php
+$catalog = array();
+$catalog[101] = "Design Patterns"; 
+$catalog[201] = "Forest Gump";
+$catalog[301] = "Beethoven";
+$catalog[102] = "Clean Code";
+
+
+
+
+
 //default value if none of condition are met
 //if there is no value set for the variable in the query string we will display this
 //which will display the full catalog, all the books,films, music. in the library
@@ -28,8 +38,22 @@ include('inc/header.php');
 
 ?>
 
-<div class="section page">
-    <h1><?php echo $pageTitle ?></h1>
+<div class="section catalog  page">
+    
+    <div class="wrapper">
+
+        <h1><?php echo $pageTitle ?></h1>
+
+        <ul>
+            <?php 
+            foreach($catalog as $item){
+                echo '<li>' . $item . '</li>';
+            }
+            ?>
+        </ul>
+
+    </div>
+
 </div>
 
 
